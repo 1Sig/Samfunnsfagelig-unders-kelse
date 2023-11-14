1c34e386c33f0e9c9e3f8808d5a9ba9277e35a3b
1Sig <Sigurdowre@gmail.com>
2023-11-14 11:00:32 +0100

    Fixed firebase connection

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-    /* Firebbas config */
-    const firebaseConfig = {
-        apiKey: "your-api-key",
-        authDomain: "your-auth-domain",
-        projectId: "your-project-id",
-        storageBucket: "your-storage-bucket",
-        messagingSenderId: "your-messaging-sender-id",
-        appId: "your-app-id"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
-
 
     const form = document.getElementById('question-form');
 
@@ -28,9 +14,9 @@
             details: details
         })
         .then(() => {
-            console.log('Question submitted successfully');
+            console.log('Form submitted successfully');
         })
         .catch((error) => {
-            console.error('Error submitting question: ', error);
+            console.error('Error submitting form: ', error);
         });
     });
