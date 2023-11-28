5feb1a96484a01321d539faf8a541da57a6522dd
1Sig <Sigurdowre@gmail.com>
2023-11-28 12:56:17 +0100

    jaa

--- script.js
+++ script.js
@@ -1,72 +1,112 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const maxPointsSpan = document.getElementById('max-points');
   const questionsContainer = document.getElementById('questions-container');
-  const pieChartCanvas = document.getElementById('pie-chart');
-  const pieChartContext = pieChartCanvas.getContext('2d');
+  const pointsLeftContainer = document.getElementById('points-left');
+  const chartContainer = document.getElementById('chart-container');
+  const legendContainer = document.getElementById('legend');
+  const chartCanvas = document.getElementById('chart');
+  const chartContext = chartCanvas.getContext('2d');
 
   // Define your questions and their default points
+  const maxPoints = 100;
   const questions = [
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
+    { points: 0 },
     { points: 0 },
     { points: 0 },
     { points: 0 },
   ];
 
-  // Set the initial max points
-  let maxPoints = 100;
-  updateMaxPoints();
-
   // Function to update points and re-render questions
   window.updatePoints = function (index, value) {
     value = parseInt(value);
 
-    // Calculate the change in points
-    const change = value - questions[index].points;
-
     // Check if the change is valid (within the maxPoints limit)
-    if (maxPoints - change >= 0) {
-      maxPoints -= change; // Deduct the change from maxPoints
-      questions[index].points = value;
-      updateMaxPoints();
-      updatePieChart();
+    if (value >= 0 && value <= 100) {
+      const currentPoints = questions.reduce((sum, question) => sum + question.points, 0);
+      const pointsLeft = maxPoints - currentPoints;
+
+      if (pointsLeft >= 0) {
+        questions[index].points = value;
+        updateChart();
+        updatePointsLeft(pointsLeft);
+      } else {
+        // If pointsLeft is negative, reset the input
+        questionsContainer.children[index].querySelector('input').value = questions[index].points;
+      }
     } else {
-      // If the change exceeds the available points, reset the input
+      // If the input is invalid, reset the input
       questionsContainer.children[index].querySelector('input').value = questions[index].points;
     }
   };
 
-  // Function to update the total points
-  function updateMaxPoints() {
-    maxPointsSpan.textContent = maxPoints;
+  // Function to update the points left counter
+  function updatePointsLeft(pointsLeft) {
+    pointsLeftContainer.textContent = pointsLeft;
   }
 
-  // Function to update the pie chart
-  function updatePieChart() {
+  // Function to update the chart
+  function updateChart() {
     // Extract labels and data from questions
     const labels = questions.map((question, index) => `Question ${index + 1}`);
     const data = questions.map(question => question.points);
 
     // Destroy the existing chart (if any)
-    if (window.pieChart) {
-      window.pieChart.destroy();
+    if (window.myChart) {
+      window.myChart.destroy();
     }
 
     // Create a new pie chart
-    window.pieChart = new Chart(pieChartContext, {
+    window.myChart = new Chart(chartContext, {
       type: 'pie',
       data: {
         labels: labels,
         datasets: [{
           data: data,
-          backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 205, 86, 0.2)'],
-          borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 205, 86, 1)'],
-          borderWidth: 1,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 159, 64, 0.7)',
+            'rgba(255, 69, 0, 0.7)',
+            'rgba(0, 128, 0, 0.7)',
+            'rgba(128, 0, 128, 0.7)',
+            'rgba(0, 0, 255, 0.7)',
+            'rgba(255, 0, 255, 0.7)',
+            'rgba(128, 128, 128, 0.7)',
+          ],
         }],
       },
+      options: {
+        legend: {
+          display: false,
+        },
+      },
     });
+
+    // Update legend
+    const legendItems = labels.map((label, index) => {
+      return `<div><span style="background-color:${window.myChart.data.datasets[0].backgroundColor[index]}"></span>${label}</div>`;
+    });
+
+    legendContainer.innerHTML = legendItems.join('');
   }
 
-  // Function to submit the form (you can customize this based on your needs)
+  // Function to submit the form
   window.submitForm = function () {
-    alert('Form submitted!'); // Add your form submission logic here
+    // Update the chart when the form is submitted
+    updateChart();
+
+    // Show the chart container and hide the questions container
+    questionsContainer.style.display = 'none';
+    chartContainer.style.display = 'block';
   };
 });
